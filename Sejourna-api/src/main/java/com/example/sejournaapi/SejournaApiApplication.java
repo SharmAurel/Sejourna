d3a2d3d0b8ac0bd9eb8cd32e3b883ba2eb5e9b55
AskMoha <79774185+AskMoha@users.noreply.github.com>
2021-12-08 15:06:24 +0100

    ajout projet spring

---
+++ Sejourna-api/src/main/java/com/example/sejournaapi/SejournaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sejournaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejournaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SejournaApiApplication.class, args);
+    }
+
+}
--- Sejourna-api/src/test/java/com/example/sejournaapi/SejournaApiApplicationTests.java
+++ Sejourna-api/src/test/java/com/example/sejournaapi/SejournaApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.Miage.Sejourna;
+package com.example.sejournaapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SejournaApplicationTests {
+class SejournaApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
