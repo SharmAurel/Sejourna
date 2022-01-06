86e47030ca149e7bddd040f0d417da7f00a327e3
SharmAurel <sharmaa.aurelien@gmail.com>
2022-01-06 13:28:29 +0100

    commit06

--- app/src/main/java/com/example/myapplication/Fragments/TripsFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/TripsFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import com.example.myapplication.R;
 
